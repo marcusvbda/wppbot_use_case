d1fc45febe0d94a6e1cea71df460e0663a10834f
Vinicius Bassalobre <=>
2021-03-17 14:00:45 -0300

    imagem e audio

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const qrcode = require('qrcode-terminal')
 const Client = require('whatsapp_engine_js')
-// const imageToBase64 = require('image-to-base64')
+const imageToBase64 = require('image-to-base64')
 const { MessageMedia } = require('whatsapp_engine_js/src/structures')
 
 const fs = require('fs')
@@ -26,36 +26,45 @@ const validateAndSendMessage = (number, cb) => {
 	})
 }
 
-const sendImageMessageTextMessage = () => {
+const sendTextMessage = () => {
 	let number = "5514996766177"
 	validateAndSendMessage(number, (account) => {
 		client.sendMessage(account.id, "foi deu certo")
 	})
 }
 
-const sendImageMessageBase64ImageMessage = () => {
+const sendBase64IimageMessage = () => {
 	let number = "5514996766177"
 	validateAndSendMessage(number, (account) => {
-		imageToBase64("./image.png").then(photo => {
+		imageToBase64("./assets/image.png").then(photo => {
 			const media = new MessageMedia('image/png', photo)
 			client.sendMessage(account.id, media, { caption: 'Here\'s your requested *media.*' })
 		}).catch(error => console.log(error))
 	})
 }
 
-const sendImageMessageImageMessage = () => {
+const sendImageMessage = () => {
 	let number = "5514996766177"
 	validateAndSendMessage(number, (account) => {
-		const media = MessageMedia.fromFilePath('./image.png')
-		client.sendMessage("5514996766177", media, { caption: "Bla bal bla" })
+		const media = MessageMedia.fromFilePath('./assets/image.png')
+		client.sendMessage(account.id, media, { caption: "Bla bal bla" })
+	})
+}
+
+const sendSendAudioMessage = () => {
+	let number = "5514996766177"
+	validateAndSendMessage(number, (account) => {
+		const media = MessageMedia.fromFilePath('./assets/audio.mp3')
+		client.sendMessage(account.id, media)
 	})
 }
 
 client.on('ready', () => {
 	console.log("client is ready")
-	sendImageMessageTextMessage()
-	// sendImageMessageImageMessage()
-	// sendImageMessageBase64ImageMessage()
+	sendSendAudioMessage()
+	sendTextMessage()
+	sendImageMessage()
+	sendBase64IimageMessage()
 })
 
 client.on('auth_failure', () => {
@@ -78,16 +87,6 @@ client.on('message_create', message => {
 
 client.on('message', async msg => {
 	console.log('message received', msg)
-	if (msg.body.toLowerCase().trim() == '!info') {
-		let info = client.info
-		client.sendMessage(msg.from, `
-            *Connection info*
-            User name: ${info.pushname}
-            My number: ${info.me.user}
-            Platform: ${info.platform}
-            WhatsApp version: ${info.phone.wa_version}
-        `)
-	}
 })
 
 client.initialize()
