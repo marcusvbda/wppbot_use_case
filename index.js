66577ddc9f776e9222e596536830e596e8baf78e
Vinicius Bassalobre <=>
2021-03-17 13:10:52 -0300

    usando a lib

--- index.js
+++ index.js
@@ -16,28 +16,45 @@ client.on('qr', qr => {
 	qrcode.generate(qr, { small: true })
 })
 
+const validateAndSendMessage = (number, cb) => {
+	client.getAccountId(number).then(account => {
+		if (account.isValid) {
+			cb(account)
+		} else {
+			console.log("Invalid Number ", number)
+		}
+	})
+}
+
 const sendImageMessageTextMessage = () => {
-	client.sendMessage("5514996766177", "foi deu certo")
+	let number = "5514996766177"
+	validateAndSendMessage(number, (account) => {
+		client.sendMessage(account.id, "foi deu certo")
+	})
 }
 
 const sendImageMessageBase64ImageMessage = () => {
-	imageToBase64("./image.png").then(photo => {
-		const media = new MessageMedia('image/png', photo)
-		client.sendMessage("5514997569008", media, { caption: 'Here\'s your requested *media.*' })
-	}).catch(error => console.log(error))
+	let number = "5514996766177"
+	validateAndSendMessage(number, (account) => {
+		imageToBase64("./image.png").then(photo => {
+			const media = new MessageMedia('image/png', photo)
+			client.sendMessage(account.id, media, { caption: 'Here\'s your requested *media.*' })
+		}).catch(error => console.log(error))
+	})
 }
 
 const sendImageMessageImageMessage = () => {
-	const media = MessageMedia.fromFilePath('./image.png')
-	client.sendMessage("5514996766177", media, { caption: "Bla bal bla" })
+	let number = "5514996766177"
+	validateAndSendMessage(number, (account) => {
+		const media = MessageMedia.fromFilePath('./image.png')
+		client.sendMessage("5514996766177", media, { caption: "Bla bal bla" })
+	})
 }
 
 client.on('ready', () => {
 	console.log("client is ready")
-	// client.sendMessage("5514997569008@c.us", 'lorem ipsum')
-
-	// sendImageMessageTextMessage()
-	sendImageMessageImageMessage()
+	sendImageMessageTextMessage()
+	// sendImageMessageImageMessage()
 	// sendImageMessageBase64ImageMessage()
 })
 
