f2db6e88814daca9b75412c60320d84034a92fa3
Vinicius Bassalobre <=>
2021-03-16 23:51:47 -0300

    save sessions

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const qrcode = require('qrcode-terminal')
 const Client = require('./src/client')
-const client = new Client()
+
+const fs = require('fs')
+const SESSION_FILE_PATH = './session.json'
+let sessionCfg
+if (fs.existsSync(SESSION_FILE_PATH)) {
+	sessionCfg = require(SESSION_FILE_PATH)
+}
+
+const client = new Client({ puppeteer: { headless: false }, session: sessionCfg })
 
 client.on('qr', qr => {
-	console.log("Scan qr code")
 	qrcode.generate(qr, { small: true })
 })
 
 client.on('ready', () => {
 	console.log('Client is ready!')
-	// let phones = ["5514996212777", "5514996216606", "5514997569008"]
-	// phones.forEach(phone => client.sendMessage(phone, 'Oi , Tudo bem ? isso é um teste'))
-
+	client.sendMessage("5514996766177", 'Oi , Tudo bem ? isso é um teste')
 })
 
-// client.on('authenticated', () => {
-// 	console.log('Authenticated')
-// })
+client.on('authenticated', (session) => {
+	console.log('Authenticated', session)
+	sessionCfg = session
+	fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+		if (err) {
+			console.error(err)
+		}
+	})
+})
 
 client.on('message_create', message => {
 	console.log(`sent message to ${message.to}`)
